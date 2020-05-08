dfb8051d872e3173469c3fd14cc8ed472025a652
Twang720 <timqwang@gmail.com>
2020-05-07 18:38:14 -0700

    bugfixes

--- bot_commands.py
+++ bot_commands.py
@@ -90,8 +90,8 @@ def split(playerData):
   dps = []
   supp = []
   
-  for name in playerData.keys: 
-    role = playerData[name]['queue']:
+  for name in playerData.keys():
+    role = playerData[name]['queue']
     if role == 'tank':
         tank.append([name, playerData[name]['tank']])
     elif role == 'dps':
@@ -208,6 +208,24 @@ def combine(tank, dps, supp):
     B.insert(0, average2/3)
         
     return [A, B]
-    
-  
+
+def main():
+    allPlayerData = {
+'usr1':   {'dps': '3944', 'support': '1698', 'tank': '3682', 'queue': 'support'},
+'usr2':   {'dps': '2970', 'support': '2282', 'tank': '1653', 'queue': 'support'},
+'usr3':   {'dps': '3439', 'support': '3516', 'tank': '1677', 'queue': 'support'},
+'usr4':   {'dps': '3606', 'support': '2407', 'tank': '3533', 'queue': 'support'},
+'usr5':   {'dps': '2072', 'support': '1778', 'tank': '3733', 'queue': 'tank'},
+'usr6':   {'dps': '2524', 'support': '1944', 'tank': '3710', 'queue': 'tank'},
+'usr7':   {'dps': '3073', 'support': '3827', 'tank': '3335', 'queue': 'tank'},
+'usr8':   {'dps': '3037', 'support': '2442', 'tank': '3254', 'queue': 'tank'},
+'usr9':   {'dps': '3817', 'support': '2766', 'tank': '3894', 'queue': 'dps'},
+'usr10':  {'dps': '2254', 'support': '2285', 'tank': '3639', 'queue': 'dps'},
+'usr11':  {'dps': '1776', 'support': '1502', 'tank': '3808', 'queue': 'dps'},
+'usr12':  {'dps': '1776', 'support': '3902', 'tank': '2488', 'queue': 'dps'}
+}
+    print(matchmake(allPlayerData))
+
+main()
+
   
