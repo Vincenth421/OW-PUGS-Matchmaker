99872e9ea7e240cb72a933dde48f1feac0e9151e
cholland4 <cameron_holland@sbcglobal.net>
2020-05-07 19:55:01 -0700

    added bot_data_functions.py

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 import discord
 import random
 from discord.ext import commands
-from bot_commands import *
+from bot_data_commands import *
 
 client = commands.Bot(command_prefix = "!")
 
 @client.event
 async def on_ready():
-        # loadPlayerData()
+        loadPlayerData()
         print("bot is ready")
+        channel = client.get_channel(707749690396901508)
+        # command testing channel
+        await channel.send("Bot Restarted.")
+        # print(clearPlayerData())
 
 @client.event
 async def on_message(message):
@@ -22,32 +26,77 @@ async def on_message(message):
 async def mention(ctx):
         await ctx.send(ctx.message.author.mention)
 
+@client.command()
+async def commands(ctx):
+        cmds = "Working commands: mention, support, tank, damage/dps, ready, sr, clear, flip/coin"
+        link = "http://blue.cs.sonoma.edu/~cholland/matchmaker.html"
+        await ctx.send(cmds + "\n" + link)
+
         
 @client.command()
 async def matchmake(ctx):
+        if numReadied < 12:
+                await ctx.send("Not enough players are readied!",
+                               "Use !ready to ready up.")
         await ctx.send("this doesn't work yet, you fool")
 
 @client.command(aliases=["support", "tank", "damage", "dps"])
 async def update(ctx):
         mystr = ctx.message.content
         sender = str(ctx.message.author)
-        updatePlayerData(mystr, sender)
-        await ctx.send("Added!")
+        if updatePlayerData(mystr, sender):
+                await ctx.send("Added!")
+        else:
+                await ctx.send("Please enter a valid integer.")
+
+@client.command(aliases=["q"])
+async def queue(ctx, role):
+        sender = str(ctx.message.author)
+        message = (queueFor(role, sender)) + "Roles Needed:\n"
+        if tankQueued() != 0:
+                message = message + (tankQueued() + " tanks.\n")
+        if dpsQueued() != 0:
+                message = message + (dpsQueued() + " dps.\n")
+        if suppQueued() != 0:
+                message = message + (suppQueued() + " supports.\n")
+        if message == "Roles Needed:\n":
+                message = "All roles filled."
+        await ctx.send(message)
+        
+@client.command()
+async def leave(ctx):
+        sender = str(ctx.message.author)
+        deQueue(sender)
+        message = "Left the queue.\n Roles Needed:\n"
+        if tankQueued() != 0:
+                message = message + (tankQueued() + " tanks.\n")
+        if dpsQueued() != 0:
+                message = message + (dpsQueued() + " dps.\n")
+        if suppQueued() != 0:
+                message = message + (suppQueued() + " supports.\n")
+        if message == "Roles Needed:\n":
+                message = "All roles filled."
+        await ctx.send(message)
 
 @client.command()
 async def sr(ctx):
-        sender = str(ctx.message.author)
-        sr = getPlayerData(sender)
-        await ctx.send(sr)
+        try:
+                sender = str(ctx.message.author)
+                sr = getPlayerData(sender)
+                await ctx.send(sr)
+        except:
+                await ctx.send("Error.")
 
 @client.command()
-async def clear(ctx, amount=1):
+async def allsr(ctx):
+        sr = getAllPlayerData()
+        await ctx.send(sr)
+       
+
+@client.command()
+async def clear(ctx, amount=5):
         if amount > 0:
                 await ctx.channel.purge(limit=amount+1)
-
-@client.command()
-async def author(ctx):
-        await ctx.send(ctx.message.author)
 
 @client.command(aliases=["coin"])
 async def flip(ctx):
---
+++ bot_data_functions.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pickle
+
+def saveTest():
+    a = playerData
+    savePlayerData()
+    loadPlayerData()
+
+def savePlayerData():
+    with open("data.pickle", "wb") as handle:
+        pickle.dump(playerData, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def loadPlayerData():
+    with open('data.pickle', 'rb') as handle:
+        playerData = pickle.load(handle)
+    return playerData
+
+playerData = loadPlayerData()
+for player in playerData:
+    playerData[player]["queue"] = "none"
+
+numQueued = {"tank":0, "damage":0, "support":0}
+
+
+def queueFor(role, PlayerID):
+    deQueue(PlayerID)
+    playerData[PlayerID]["queue"] = role
+    
+    if role == "tank":
+        numQueued["tank"] += 1
+        return ("Queued for tank.\n")
+    elif role == "damage" or role == "dps":
+        numQueued["damage"] += 1
+        return ("Queued for dps.\n")
+    elif role == "support":
+        numQueued["support"] += 1
+        return ("Queued for support.\n")
+    elif role == "none":
+        deQueue(PlayerID)
+        return ("Left the queue.\n")
+    else:
+        return("Invalid role.\n")
+
+    
+
+def suppQueued():
+    if numQueued["support"] < 4:
+        numNeeded = 4 - numQueued["support"]
+        return str(numNeeded)
+    else:
+        return 0
+    
+def tankQueued():
+    if numQueued["tank"] < 4:
+        numNeeded = 4 - numQueued["tank"]
+        return str(numNeeded)
+    else:
+        return 0
+
+def dpsQueued():
+    if numQueued["damage"] < 4:
+        numNeeded = 4 - numQueued["damage"]
+        return str(numNeeded)
+    else:
+        return 0
+
+def deQueue(PlayerID):
+    role = playerData[PlayerID]["queue"]
+    playerData[PlayerID]["queue"] = "none"
+    if role == "tank":
+        numQueued["tank"] -= 1
+    elif role == "damage" or role == "dps":
+        numQueued["damage"] -= 1
+    elif role == "support":
+        numQueued["support"] -= 1
+    
+    
+#good work gang
+def updatePlayerData(mystr, PlayerID):
+    playerData[player]["queue"] = "none"
+    userData = mystr.split()
+    if userData[1].isalpha():
+        return False
+    sr = int(userData[1])
+    if sr < 0:
+        return False
+    if PlayerID not in playerData:
+        playerData[PlayerID] = {}
+    if(userData[0] == "!support"):
+        playerData[PlayerID]["support"] = sr
+    elif(userData[0] == "!damage" or userData[0] == "!dps"):
+        playerData[PlayerID]["dps"] = sr
+    elif(userData[0] == "!tank"):
+        playerData[PlayerID]["tank"] = sr
+    print(playerData)
+    savePlayerData()
+    return True
+
+
+def clearPlayerData():
+    playerData.clear()
+    savePlayerData()
+    return playerData
+
+
+def getPlayerData(PlayerID):
+    # I'd like for this to return a nice and cleanly formatted string with
+    # the player data
+    return playerData[PlayerID]
+
+def getAllPlayerData():
+    return playerData
