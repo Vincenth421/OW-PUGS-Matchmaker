211604cc26bf9364f88cc2741a85843f396b5167
Twang720 <timqwang@gmail.com>
2020-05-07 19:03:56 -0700

    working version of matchmaking algo

--- bot_commands.py
+++ bot_commands.py
@@ -66,7 +66,7 @@ def getPlayerData(PlayerID):
     # I'd like for this to return a nice and cleanly formatted string with
     # the player data
     return playerData[PlayerID]
-        
+
 # main matchmaking function
 # chooses 12 players, splits into roles, matchmakes roles, and then combines them back together
 def matchmake(playerData):
@@ -76,39 +76,38 @@ def matchmake(playerData):
     dps = roles[1]
     supp = roles[2]
     
-    t = matchmake(tank)
-    d = matchmake(dps)
-    s = matchmake(supp)
+    t = balance(tank)
+    d = balance(dps)
+    s = balance(supp)
     
     return combine(t, d, s)
 
 # ready is a bool
-# playerData is a hash table of any number fo people
+# playerData is a hash table of any number of people
 # splits all players into their chosen roles, then selects 4 for each role
 def split(playerData):
-  tank = []
-  dps = []
-  supp = []
-  
-  for name in playerData.keys():
-    role = playerData[name]['queue']
-    if role == 'tank':
-        tank.append([name, playerData[name]['tank']])
-    elif role == 'dps':
-        dps.append([name, playerData[name]['dps']])
-    elif role == 'support':
-        supp.append([name, playerData[name]['support']])
-  
-    return [select(tank), select(dps), select(supp)]
+    tank = []
+    dps = []
+    supp = []
+    
+    for name in playerData.keys():
+        role = playerData[name]['queue']
+        if role == 'tank':
+            tank.append([name, playerData[name]['tank']])
+        elif role == 'dps':
+            dps.append([name, playerData[name]['dps']])
+        elif role == 'support':
+            supp.append([name, playerData[name]['support']])
+
+return [select(tank), select(dps), select(supp)]
 
 # selects 4 players from a pool of any number
 # role comes in as a list, returns a list of the randomly selected players
 def select(role):
     selected = []
-    for i in range(4):
-        num = random.randint(0, len(role)-1)
-        #TODO check for dupe random numbers
-        selected.append(role[num])
+    nums = np.random.choice(len(role), 4, replace=False)
+    for i in range(len(nums)):
+        selected.append(role[i])
     return selected
 
 # given a role hash table
@@ -123,9 +122,9 @@ def balance(role):
     totalsr = 0
     for i in range(len(role)):
         totalsr += role[i][1]
-
+    
     for i in range(len(role)):
-        for j in range(i, len(role)):
+        for j in range(i+1, len(role)):
             avg1 = (role[i][1] + role[j][1]) / 2
             avg2 = (totalsr - role[i][1] - role[j][1]) / 2
             difference = avg1 - avg2
@@ -133,12 +132,12 @@ def balance(role):
                 average2 = avg2
                 bestDifference = difference
                 bestPair = [avg1, role[i][0], role[j][0]]
-  
-    otherPair = [average2]
-    for i in range(len(role)):
-        if ~(role[i][0] in bestPair):
-            otherPair.append(role[i][0])
-  
+
+otherPair = [average2]
+for i in range(len(role)):
+    if not (role[i][0] in bestPair):
+        otherPair.append(role[i][0])
+    
     both = [bestPair, otherPair]
     return both
 
@@ -158,7 +157,7 @@ def combine(tank, dps, supp):
     average2 = tank[1][0]
     A = [tank[0][1], tank[0][2]]
     B = [tank[1][1], tank[1][2]]
-  
+    
     # brute force calculation of combined sr average
     bestDiff = tankDiff + dpsDiff + suppDiff
     if abs(tankDiff - dpsDiff + suppDiff) < abs(bestDiff):
@@ -202,30 +201,33 @@ def combine(tank, dps, supp):
         B.append(supp[1][2])
         average1 += supp[0][0]
         average2 += supp[1][0]
-    
+
     # cameron's code, if buggy blame him
     A.insert(0, average1/3)
     B.insert(0, average2/3)
-        
-    return [A, B]
+
+return [A, B]
 
 def main():
     allPlayerData = {
-'usr1':   {'dps': '3944', 'support': '1698', 'tank': '3682', 'queue': 'support'},
-'usr2':   {'dps': '2970', 'support': '2282', 'tank': '1653', 'queue': 'support'},
-'usr3':   {'dps': '3439', 'support': '3516', 'tank': '1677', 'queue': 'support'},
-'usr4':   {'dps': '3606', 'support': '2407', 'tank': '3533', 'queue': 'support'},
-'usr5':   {'dps': '2072', 'support': '1778', 'tank': '3733', 'queue': 'tank'},
-'usr6':   {'dps': '2524', 'support': '1944', 'tank': '3710', 'queue': 'tank'},
-'usr7':   {'dps': '3073', 'support': '3827', 'tank': '3335', 'queue': 'tank'},
-'usr8':   {'dps': '3037', 'support': '2442', 'tank': '3254', 'queue': 'tank'},
-'usr9':   {'dps': '3817', 'support': '2766', 'tank': '3894', 'queue': 'dps'},
-'usr10':  {'dps': '2254', 'support': '2285', 'tank': '3639', 'queue': 'dps'},
-'usr11':  {'dps': '1776', 'support': '1502', 'tank': '3808', 'queue': 'dps'},
-'usr12':  {'dps': '1776', 'support': '3902', 'tank': '2488', 'queue': 'dps'}
+'usr1':   {'dps': 3944, 'support': 1698, 'tank': 3682, 'queue': 'support'},
+'usr2':    {'dps': 2970, 'support': 2282, 'tank': 1653, 'queue': 'support'},
+'usr3':   {'dps': 3439, 'support': 3516, 'tank': 1677, 'queue': 'support'},
+'usr4':   {'dps': 3606, 'support': 2407, 'tank': 3533, 'queue': 'support'},
+'usr5':   {'dps': 2072, 'support': 1778, 'tank': 3733, 'queue': 'tank'},
+'usr6':   {'dps': 2524, 'support': 1944, 'tank': 3710, 'queue': 'tank'},
+'usr7':   {'dps': 3073, 'support': 3827, 'tank': 3335, 'queue': 'tank'},
+'usr8':   {'dps': 3037, 'support': 2442, 'tank': 3254, 'queue': 'tank'},
+'usr9':   {'dps': 3817, 'support': 2766, 'tank': 3894, 'queue': 'dps'},
+'usr10':  {'dps': 2254, 'support': 2285, 'tank': 3639, 'queue': 'dps'},
+'usr11':  {'dps': 1776, 'support': 1502, 'tank': 3808, 'queue': 'dps'},
+'usr12':  {'dps': 1776, 'support': 3902, 'tank': 2488, 'queue': 'dps'}
 }
+    
     print(matchmake(allPlayerData))
 
 main()
+
+
 
   
